107a0689a89ee4a78d3e8c3a1ae5f3b1e808ddcb
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-16 22:20:36 +0530

    Initial commit

---
+++ src/main/java/com/college/teamcollab/TeamcollabApplication.java
@@ -0,0 +1,13 @@
+package com.college.teamcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/college/teamcollab/TeamcollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.teamcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
