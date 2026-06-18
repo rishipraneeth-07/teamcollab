ec8f0e427a5a5970f77c5c6577cd6690ce0f3f48
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-18 11:39:34 +0530

    feat: add LoginRequest DTO with validation

---
+++ src/main/java/com/college/teamcollab/dto/auth/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.college.teamcollab.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
