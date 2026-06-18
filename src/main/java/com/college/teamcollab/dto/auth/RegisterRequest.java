96fade26a5477104f22ae222659f8b8950fccb63
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-18 11:35:22 +0530

    feat: add RegisterRequest DTO with validation

---
+++ src/main/java/com/college/teamcollab/dto/auth/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.college.teamcollab.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+}
