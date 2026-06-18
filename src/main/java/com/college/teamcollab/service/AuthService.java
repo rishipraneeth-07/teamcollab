f8bd0bf3ccf8e575191c0ccc15e95830988af148
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-18 12:09:16 +0530

    feat: create AuthService with dependency injection

---
+++ src/main/java/com/college/teamcollab/service/AuthService.java
@@ -0,0 +1,16 @@
+package com.college.teamcollab.service;
+
+import com.college.teamcollab.repo.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
