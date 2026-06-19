d4baa6ceb790e513edcf653887ecd0ff8ae23b33
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-19 11:23:25 +0530

    feat: complete user registration API

---
+++ src/main/java/com/college/teamcollab/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.college.teamcollab.controller;
+
+import com.college.teamcollab.dto.auth.RegisterRequest;
+import com.college.teamcollab.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        String message =authService.register(registerRequest);
+        return  ResponseEntity.ok(message);
+    }
+
+}
