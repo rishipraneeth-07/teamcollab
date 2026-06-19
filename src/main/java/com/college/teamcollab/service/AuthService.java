4706c32c5ded2475c9f3736a6cba4a44ab7a616e
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-19 10:55:35 +0530

    feat: implement user registration service

--- src/main/java/com/college/teamcollab/service/AuthService.java
+++ src/main/java/com/college/teamcollab/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.college.teamcollab.service;
 
+import com.college.teamcollab.dto.auth.RegisterRequest;
+import com.college.teamcollab.entity.User;
 import com.college.teamcollab.repo.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,23 @@ public class AuthService {
     public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public String register(RegisterRequest registerRequest) {
+        if(userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        if(userRepository.existsByUsername(registerRequest.getUsername())) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+        String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
+
+        User user = User.builder()
+                .username(registerRequest.getUsername())
+                .email(registerRequest.getEmail())
+                .password(encodedPassword)
+                .build();
+        userRepository.save(user);
+        return "User registered successfully";
     }
 }
