f6a0c60051cd982b223f3f40ec020714602cc5f8
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-22 10:37:06 +0530

    feat: configure public auth endpoints in security

--- src/main/java/com/college/teamcollab/config/SecurityConfig.java
+++ src/main/java/com/college/teamcollab/config/SecurityConfig.java
@@ -2,14 +2,28 @@ package com.college.teamcollab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
     }
 
 }
