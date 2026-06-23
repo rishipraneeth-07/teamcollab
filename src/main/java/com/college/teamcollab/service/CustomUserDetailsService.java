2690872af0f825b0ae8aede0a5dd16948a720ce9
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-23 11:14:09 +0530

    feat: implement custom user details service

---
+++ src/main/java/com/college/teamcollab/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.college.teamcollab.service;
+
+import com.college.teamcollab.entity.User;
+import com.college.teamcollab.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
