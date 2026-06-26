9dfad4ead2a374b0c4ec5838582954a01ff394f5
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-26 11:37:57 +0530

    feat: add UpdateChannelRequest

---
+++ src/main/java/com/college/teamcollab/dto/channel/UpdateChannelRequest.java
@@ -0,0 +1,20 @@
+package com.college.teamcollab.dto.channel;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateChannelRequest {
+    @NotBlank(message = "Channel name is required")
+    @Size(min = 2, max = 50, message = "Channel name must be between 2 and 50 characters")
+    private String name;
+
+    @Size(max = 200, message = "Description cannot exceed 200 characters")
+    private String description;
+
+}
