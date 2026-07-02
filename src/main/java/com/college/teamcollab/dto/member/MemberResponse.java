d12144d10d158b87eb55f2381a75b85065cb5965
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-02 22:00:38 +0530

    feat(member): add member response dto

---
+++ src/main/java/com/college/teamcollab/dto/member/MemberResponse.java
@@ -0,0 +1,18 @@
+package com.college.teamcollab.dto.member;
+
+import com.college.teamcollab.entity.ChannelRole;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberResponse {
+    private Long id;
+    private String username;
+    private ChannelRole role;
+    private LocalDateTime joinAt;
+}
