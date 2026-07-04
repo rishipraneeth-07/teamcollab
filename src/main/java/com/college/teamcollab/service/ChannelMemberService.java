ce5dc2cfe6394a2efff3a43d60b32b6f1c4ac4e7
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-04 09:58:07 +0530

    feat(member): add channelmember Service

---
+++ src/main/java/com/college/teamcollab/service/ChannelMemberService.java
@@ -0,0 +1,15 @@
+package com.college.teamcollab.service;
+
+import com.college.teamcollab.dto.channel.ChannelResponse;
+import com.college.teamcollab.dto.member.MemberResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ChannelMemberService {
+    MemberResponse joinChannel(Long channelId);
+    void leaveChannel(Long channelId);
+    List<MemberResponse> getMembers(Long channelId);
+    Page<ChannelResponse> getMyChannels(Pageable pageable);
+}
