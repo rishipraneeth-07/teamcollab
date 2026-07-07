0dbd7ef5ef20163fe6700952fde584bb2a2bd735
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-07 09:11:18 +0530

    feat(message): add message repository

---
+++ src/main/java/com/college/teamcollab/repo/MessageRepository.java
@@ -0,0 +1,16 @@
+package com.college.teamcollab.repo;
+
+import com.college.teamcollab.entity.Channel;
+import com.college.teamcollab.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findByChannelOrderByCreatedAtDesc(Channel channel,Pageable pageable);
+
+    }
+
