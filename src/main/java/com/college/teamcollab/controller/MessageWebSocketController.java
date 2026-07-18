6f0796170125d0809e493274b9fde5620b535adb
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-18 17:58:58 +0530

    fix(websocket): add annotations and fixed destination path

--- src/main/java/com/college/teamcollab/controller/MessageWebSocketController.java
+++ src/main/java/com/college/teamcollab/controller/MessageWebSocketController.java
@@ -5,26 +5,26 @@ import com.college.teamcollab.dto.message.CreateMessageRequest;
 import com.college.teamcollab.dto.message.MessageResponse;
 import com.college.teamcollab.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class MessageWebSocketController {
-    private final MessageService  messageService;
+
+    private final MessageService messageService;
     private final SimpMessagingTemplate messagingTemplate;
 
+    @MessageMapping("/chat.send")
     public void sendMessage(ChatMessage chatMessage) {
-        CreateMessageRequest request =CreateMessageRequest
-                .builder()
+        CreateMessageRequest request = CreateMessageRequest.builder()
                 .content(chatMessage.getContent())
                 .build();
-
-        MessageResponse response=messageService.sendMessage(chatMessage.getChannelId(), request);
-        messagingTemplate.convertAndSend("/topic/channels"+chatMessage.getChannelId(), response);
-
-
-
+        MessageResponse response = messageService.sendMessage(chatMessage.getChannelId(), request);
+        messagingTemplate.convertAndSend(
+                "/topic/channels/" + chatMessage.getChannelId(),
+                response
+        );
     }
-
 }
