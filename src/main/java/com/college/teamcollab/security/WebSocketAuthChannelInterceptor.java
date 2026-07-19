ec10f0ad65efdb71f41371d89c5c1ab9e3bb9bd3
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-19 21:38:14 +0530

    feat(websocket): add websocketauthchannel interceptor

--- src/main/java/com/college/teamcollab/security/WebSocketAuthChannelInterceptor.java
+++ src/main/java/com/college/teamcollab/security/WebSocketAuthChannelInterceptor.java
@@ -1,4 +1,45 @@
 package com.college.teamcollab.security;
 
-public class WebSocketAuthChannelInterceptor {
+import com.college.teamcollab.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketAuthChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String authHeader =accessor.getFirstNativeHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                if (jwtUtil.validateToken(token)) {
+                    String email = jwtUtil.extractEmail(token);
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+                    accessor.setUser(authentication);
+                }
+            }
+        }
+        return message;
+    }
 }
