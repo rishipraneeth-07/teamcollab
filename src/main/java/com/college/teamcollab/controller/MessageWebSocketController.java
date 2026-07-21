d8bc892d724c5ba4087459262a3a55708b4e6aee
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-21 22:21:41 +0530

    fix(websocket): updated messageWebSocket Controller

--- src/main/java/com/college/teamcollab/controller/MessageWebSocketController.java
+++ src/main/java/com/college/teamcollab/controller/MessageWebSocketController.java
@@ -5,9 +5,12 @@ import com.college.teamcollab.dto.message.CreateMessageRequest;
 import com.college.teamcollab.dto.message.MessageResponse;
 import com.college.teamcollab.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,11 +20,25 @@ public class MessageWebSocketController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.send")
-    public void sendMessage(ChatMessage chatMessage) {
+    public void sendMessage(
+            ChatMessage chatMessage,
+            @Header("simpSessionAttributes")
+            Map<String, Object> sessionAttributes
+    ) {
+
+        String email = (String) sessionAttributes.get("email");
+
         CreateMessageRequest request = CreateMessageRequest.builder()
                 .content(chatMessage.getContent())
                 .build();
-        MessageResponse response = messageService.sendMessage(chatMessage.getChannelId(), request);
+
+        MessageResponse response =
+                messageService.sendMessage(
+                        email,
+                        chatMessage.getChannelId(),
+                        request
+                );
+
         messagingTemplate.convertAndSend(
                 "/topic/channels/" + chatMessage.getChannelId(),
                 response
